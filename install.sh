38bc239051c4a62738686f7d33eb18c16ecdc6da
Erik Smith <erik.smith@jetblue.com>
2024-05-22 12:41:17 -0400

    Add new aliases

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
+git config --global alias.log1 'log --oneline'
 git config --global alias.l1 'log --oneline'
+# list all aliases
+git config --global alias.aliases 'config --get-regexp ^alias'
+# Checkout -b
+git config --global alias.cob 'checkout -b'
+# Commit all with message
+git config --global alias.coma 'commit -am'
