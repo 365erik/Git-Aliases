8ae87ee47d4f2004632b6e97bd326ac0383d6430
Erik Smith <erik.smith@jetblue.com>
2024-05-22 15:42:18 -0400

    feat: add more aliases

--- install.sh
+++ install.sh
@@ -4,5 +4,12 @@ git config --global alias.l1 'log --oneline'
 git config --global alias.aliases 'config --get-regexp ^alias'
 # Checkout -b
 git config --global alias.cob 'checkout -b'
-# Commit all with message
+# Commit all changes with message
 git config --global alias.coma 'commit -am'
+# Show current branch
+git config --global alias.whereami 'branch --show-current'
+# Add -p
+git config --global alias.ap 'add -p'
+# status
+git config --global alias.s 'status'
+git config --global alias.sp 'status --porcelain'
