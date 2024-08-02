67861ddf6a50a0491afa49aa47d59da400012b58
Erik Smith <erik.smith@jetblue.com>
2024-08-02 12:17:07 -0400

    Add more commands

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ git config --global alias.aliases 'config --get-regexp ^alias'
 git config --global alias.cob 'checkout -b'
 # Commit all changes with message
 git config --global alias.coma 'commit -am'
+# Make an empty commit
+git config --global alias.empty 'commit --allow-empty -m'
 # Show current branch
 git config --global alias.whereami 'branch --show-current'
 # Add -p
@@ -13,5 +15,7 @@ git config --global alias.ap 'add -p'
 # status
 git config --global alias.s 'status'
 git config --global alias.sp 'status --porcelain'
+# new
+git config --global alias.new "!f() { git checkout main && git pull origin main && git checkout -b \"\$1\" && git commit --allow-empty -m \"First commit on \$1\"; }; f"
 # scratch
-git config --global alias.test '!echo "test stuff here"'
+git config --global alias.scratch "!f() { echo \"Hello, World\"; }; f"
